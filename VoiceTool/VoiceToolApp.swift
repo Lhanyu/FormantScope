fed2ed60bd6bbf392546522257325d17e23b4be9
hanyu <hanyu2333@gmail.com>
2026-04-25 22:06:40 +0800

    Initial Commit

---
+++ VoiceTool/VoiceToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceToolApp.swift
+//  VoiceTool
+//
+//  Created by Hanyu on 2026/4/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceToolTests/VoiceToolTests.swift
@@ -0,0 +1,16 @@
+//
+//  VoiceToolTests.swift
+//  VoiceToolTests
+//
+//  Created by Hanyu on 2026/4/25.
+//
+
+import Testing
+
+struct VoiceToolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
